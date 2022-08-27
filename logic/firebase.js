9527dadd0ec5b968cc9322fe4745f7bda57814b1
Yasmim Fernandes <105113571+yasmim3001@users.noreply.github.com>
2022-08-27 10:34:11 -0300

    upload

---
+++ logic/firebase.js
@@ -0,0 +1,13 @@
+// insira suas credenciais aqui
+ const firebaseConfig = {
+    apiKey: "AIzaSyD-2jyUGnERVabQDFPrfNOeDPu8XAww5Os",
+    authDomain: "to-do-list-yasmim.firebaseapp.com",
+    databaseURL: "https://to-do-list-yasmim-default-rtdb.firebaseio.com",
+    projectId: "to-do-list-yasmim",
+    storageBucket: "to-do-list-yasmim.appspot.com",
+    messagingSenderId: "113301107279",
+    appId: "1:113301107279:web:7465ef23243ffd39b2b03a"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
